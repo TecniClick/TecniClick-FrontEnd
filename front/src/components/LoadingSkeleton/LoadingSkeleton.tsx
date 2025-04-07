9438f3a09ca8436257f094413e0fc145b55e1e2e
Santiago-Taborda <santy204bt@gmail.com>
2025-04-07 20:27:16 -0300

    aplicado de modo oscuro en la mayoria de paginas

--- front/src/components/LoadingSkeleton/LoadingSkeleton.tsx
+++ front/src/components/LoadingSkeleton/LoadingSkeleton.tsx
@@ -1,22 +1,17 @@
 import Image from "next/image";
 import logo from "../../../public/logo.png";
+import logoContraste from "../../../public/logoContraste.png";
 
 export default function LoadingSkeleton() {
     return (
-        <div className="flex flex-col items-center justify-center min-h-screen bg-primary">
+        <div className="flex flex-col items-center justify-center min-h-screen">
         <div className="animate-pulse">
-          <Image
-            src={logo}
-            alt="Logo de la empresa"
-            width={120}
-            height={120}
-            priority
-          />
+          {/* Logo para modo claro */}
+          <Image src={logoContraste} alt="logo claro" className="w-32 sm:w-64 block dark:hidden" priority/>
+          {/* Logo para modo oscuro */}
+          <Image src={logo} alt="logo oscuro" className="w-32 sm:w-64 hidden dark:block" priority/>
         </div>
-  
-        <p className="mt-4 text-secondary text-lg animate-pulse">
-          Cargando...
-        </p>
+        <p className="mt-4 text-lg animate-pulse">Cargando...</p>
       </div>
     );
 };
--- front/src/components/ServicesPage/ServicesPage.tsx
+++ front/src/components/ServicesPage/ServicesPage.tsx
@@ -1,5 +1,9 @@
 "use client";
-import { getFilteredServices, getServiceProfile, getServiceProfileByCategory } from "@/services/profileService";
+import {
+  getFilteredServices,
+  getServiceProfile,
+  getServiceProfileByCategory,
+} from "@/services/profileService";
 import { useSearchParams } from "next/navigation";
 import React, { useEffect, useState } from "react";
 import SearchBar from "../SearchBar/SearchBar";
@@ -10,141 +14,129 @@ import profile from "../../../public/profile.png";
 import Link from "next/link";
 
 const ServicesPage: React.FC = () => {
-    const searchParams = useSearchParams();
-    const searchQuery = searchParams.get("Buscar Servicio") || "";
+  const searchParams = useSearchParams();
+  const searchQuery = searchParams.get("Buscar Servicio") || "";
 
-    const [services, setServices] = useState<ServiceProfileType[]>([]);
-    const [filteredServices, setFilteredServices] = useState<ServiceProfileType[]>([]);
-    const [categories, setCategories] = useState<CategoryType[]>([]);
-    const [nameCategory, setNameCategory] = useState<string | null>(null);
-    const [sortPrice, setSortPrice] = useState<string | null>(null);
+  const [services, setServices] = useState<ServiceProfileType[]>([]);
+  const [filteredServices, setFilteredServices] = useState<ServiceProfileType[]>([]);
+  const [categories, setCategories] = useState<CategoryType[]>([]);
+  const [nameCategory, setNameCategory] = useState<string | null>(null);
+  const [sortPrice, setSortPrice] = useState<string | null>(null);
 
-    useEffect(() => {
-        const fetchCategories = async () => {
-            const categoriesList = await getCategories();
-            setCategories(categoriesList);
-        };
-        fetchCategories();
-    }, []);
-
-    useEffect(() => {
-        const fetchServices = async () => {
-            const allServices = await getServiceProfile();
-            setServices(allServices);
-            setFilteredServices(allServices);
-        };
-        fetchServices();
-    }, []);
-
-    useEffect(() => {
-        const fetchFilteredServices = async () => {
-            let updatedServices = services;
-
-            if (searchQuery) {
-                updatedServices = await getFilteredServices(searchQuery);
-            } else if (nameCategory) {
-                updatedServices = await getServiceProfileByCategory(nameCategory) || [];
-            }
-
-            if (sortPrice === "asc") {
-                updatedServices = [...updatedServices].sort((a, b) => a.price - b.price);
-            } else if (sortPrice === "desc") {
-                updatedServices = [...updatedServices].sort((a, b) => b.price - a.price);
-            }
-
-            setFilteredServices(updatedServices);
-        };
-        fetchFilteredServices();
-    }, [searchQuery, nameCategory, services, sortPrice]);
-
-    const resetFilters = () => {
-        setNameCategory(null);
-        setSortPrice(null);
-        setFilteredServices(services);
+  useEffect(() => {
+    const fetchCategories = async () => {
+      const categoriesList = await getCategories();
+      setCategories(categoriesList);
     };
+    fetchCategories();
+  }, []);
 
-    return (
-        <div className="max-w-7xl mx-auto p-4 md:p-6 min-h-screen text-tertiary">
-            <div className="flex items-center justify-center w-full mb-4">
-                <SearchBar onSearch={async (query) => {
-                    const services = await getFilteredServices(query);
-                    setFilteredServices(services);
-                }} />
-            </div>
+  useEffect(() => {
+    const fetchServices = async () => {
+      const allServices = await getServiceProfile();
+      setServices(allServices);
+      setFilteredServices(allServices);
+    };
+    fetchServices();
+  }, []);
 
-            <div className="flex flex-wrap items-center justify-center gap-4 my-4">
-                <select
-                    className="border p-2 rounded-lg w-full sm:w-auto"
-                    onChange={(e) => setNameCategory(e.target.value || null)}
-                    value={nameCategory || ""}
-                >
-                    <option value="">Todas las categorías</option>
-                    {categories.map(category => (
-                        <option key={category.id} value={category.id}>{category.name}</option>
-                    ))}
-                </select>
+  useEffect(() => {
+    const fetchFilteredServices = async () => {
+      let updatedServices = services;
 
-                <select
-                    className="border p-2 rounded-lg w-full sm:w-auto"
-                    onChange={(e) => setSortPrice(e.target.value || null)}
-                    value={sortPrice || ""}
-                >
-                    <option value="">Ordenar por precio</option>
-                    <option value="asc">Menor a Mayor</option>
-                    <option value="desc">Mayor a Menor</option>
-                </select>
+      if (searchQuery) {
+        updatedServices = await getFilteredServices(searchQuery);
+      } else if (nameCategory) {
+        updatedServices = (await getServiceProfileByCategory(nameCategory)) || [];
+      }
 
-                <button
-                    className="bg-quaternary text-white px-4 py-2 rounded-lg hover:bg-opacity-80 w-full sm:w-auto"
-                    onClick={resetFilters}
-                >
-                    Limpiar Filtros
-                </button>
-            </div>
+      if (sortPrice === "asc") {
+        updatedServices = [...updatedServices].sort((a, b) => a.price - b.price);
+      } else if (sortPrice === "desc") {
+        updatedServices = [...updatedServices].sort((a, b) => b.price - a.price);
+      }
 
-            <ul className="grid gap-4 mx-auto grid-cols-1 sm:grid-cols-2 lg:grid-cols-">
-                {filteredServices.length > 0 ? (
-                    filteredServices.map((service) => (
-                        <li
-                            key={service.id}
-                            className="bg-secondary border p-4 rounded-lg shadow-lg"
-                        >
-                            <Link href={`/services/${service.userId}`}>
-                                <div className="flex items-center gap-2 cursor-pointer">
-                                    <Image
-                                        src={profile}
-                                        alt="Foto de perfil"
-                                        width={50}
-                                        className="rounded-full"
-                                    />
-                                    <h3 className="text-lg font-semibold">{service.name}</h3>
-                                </div>
-                                <div className="p-1">
-                                    <p className="text-tertiary">
-                                        Profesional: <span className="font-bold">{service.user}</span>
-                                    </p>
-                                    <p className="text-tertiary">
-                                        Categoría: <span className="font-bold">{service.category ? service.category.name : "Categoría no disponible"}</span>
-                                    </p>
-                                    <p className="text-tertiary">
-                                        Precio Base: <span className="font-bold">${service.price}</span>
-                                    </p>
-                                    <p className="text-tertiary">Descripción: {service.description}.</p>
-                                    <p className="text-sm text-tertiary">Puntuación: {service.rating}</p>
-                                </div>
-                            </Link>
-                        </li>
-                    ))
-                ) : (
-                    <div className="bg-secondary p-4 border rounded-lg shadow-lg text-center flex items-center justify-center col-span-full">
-                        <p className="text-tertiary">
-                            No hay profesionales disponibles aún en esta categoría.
-                        </p>
-                    </div>
-                )}
-            </ul>
-        </div>
-    );
+      setFilteredServices(updatedServices);
+    };
+    fetchFilteredServices();
+  }, [searchQuery, nameCategory, services, sortPrice]);
+
+  const resetFilters = () => {
+    setNameCategory(null);
+    setSortPrice(null);
+    setFilteredServices(services);
+  };
+
+  return (
+    <div className="max-w-7xl mx-auto p-4 md:p-6 min-h-screen text-tertiary">
+      <div className="flex items-center justify-center w-full mb-4">
+        <SearchBar
+          onSearch={async (query) => {
+            const services = await getFilteredServices(query);
+            setFilteredServices(services);
+          }}
+        />
+      </div>
+
+      <div className="flex flex-wrap items-center justify-center gap-4 my-4">
+        <select
+          className="border p-2 rounded-lg w-full sm:w-auto"
+          onChange={(e) => setNameCategory(e.target.value || null)}
+          value={nameCategory || ""}
+        >
+          <option value="">Todas las categorías</option>
+          {categories.map((category) => (
+            <option key={category.id} value={category.id}>{category.name}</option>
+          ))}
+        </select>
+
+        <select
+          className="border p-2 rounded-lg w-full sm:w-auto"
+          onChange={(e) => setSortPrice(e.target.value || null)}
+          value={sortPrice || ""}
+        >
+          <option value="">Ordenar por precio</option>
+          <option value="asc">Menor a Mayor</option>
+          <option value="desc">Mayor a Menor</option>
+        </select>
+
+        <button
+          className="bg-quaternary text-white px-4 py-2 rounded-lg hover:bg-opacity-80 w-full sm:w-auto"
+          onClick={resetFilters}
+        >Limpiar Filtros</button>
+      </div>
+
+      <ul className="grid gap-4 mx-auto grid-cols-1 sm:grid-cols-2 lg:grid-cols-">
+        {filteredServices.length > 0 ? (
+          filteredServices.map((service) => (
+            <li key={service.id} className="oscuro border p-4 rounded-lg shadow-lg">
+              <Link href={`/services/${service.userId}`}>
+                <div className="flex items-center gap-2 cursor-pointer">
+                  <Image src={profile} alt="Foto de perfil" width={50} className="rounded-full border-2 border-quaternary dark:border-quinary" />
+                  <h3 className="text-lg font-semibold">{service.name}</h3>
+                </div>
+                <div className="p-1">
+                  <p>Profesional: <span className="font-bold">{service.user}</span></p>
+                  <p>Categoría:{" "}
+                    <span className="font-bold">
+                      {service.category ? service.category.name : "Categoría no disponible"}
+                    </span>
+                  </p>
+                  <p>Precio Base: <span className="font-bold">${service.price}</span></p>
+                  <p>Descripción: {service.description}.</p>
+                  <p className="text-sm">Puntuación: {service.rating}</p>
+                </div>
+              </Link>
+            </li>
+          ))
+        ) : (
+          <div className="bg-secondary p-4 border rounded-lg shadow-lg text-center flex items-center justify-center col-span-full">
+            <p className="text-tertiary">No hay profesionales disponibles aún en esta categoría.</p>
+          </div>
+        )}
+      </ul>
+    </div>
+  );
 };
 
 export default ServicesPage;
