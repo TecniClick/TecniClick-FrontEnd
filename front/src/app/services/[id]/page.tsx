e9a1b6d61caf9ec6d7c715b21771ffabc64b291d
Leandro Miguel Gianelli Louis <lean.louis1997@gmail.com>
2025-04-21 02:24:21 -0300

    deploy commit

--- front/src/app/services/[id]/page.tsx
+++ front/src/app/services/[id]/page.tsx
@@ -9,41 +9,46 @@ interface ProductParams {
 export async function generateMetadata(
   { params }: { params: ProductParams }
 ): Promise<Metadata> {
-  const user = await getServiceProfileById(params.id);
-
-  if (!user) {
+  try {
+    const user = await getServiceProfileById(params.id);
+    if (!user) {
+      return {
+        title: "Proveedor no encontrado | TecniClick",
+      };
+    }
     return {
-      title: "Proveedor no encontrado | TecniClick",
+      title: `${user.userName} | TecniClick`,
+    };
+  } catch (error) {
+    console.error("Error al obtener los datos del proveedor:", error);
+    return {
+      title: "Proveedor no disponible | TecniClick",
     };
   }
-
-  return {
-    title: `${user.userName} | TecniClick`,
-  };
 }
 
-
-const SlugProduct = async ({ params }: { params: ProductParams }) => {
-
-  const { id } = await params;
+export async function Page({ params }: { params: ProductParams }) {
+  const { id } = params;
   console.log("params:", params);
   console.log("ID recibido en SlugProduct:", id);
 
-  const user = await getServiceProfileById(id)
+  let user;
+  try {
+    user = await getServiceProfileById(id);
+  } catch (error) {
+    console.error("Error al obtener el perfil del proveedor:", error);
+    user = null; // O manejar el error de alguna otra forma.
+  }
 
   return (
     <div className="mx-[4%]">
       {user ? (
         <div className="flex justify-center items-center">
-          <ServiceProviderUser {...user}
-          />
-
+          <ServiceProviderUser {...user} />
         </div>
       ) : (
         <h2 className="w-full pt-[20vh] text-center">Usuario no encontrado</h2>
       )}
     </div>
   );
-};
-
-export default SlugProduct;
+}
